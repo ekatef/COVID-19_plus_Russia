2e780d42c9486693e5824c2e96a106d153b42264
Sergey Mironov <grrwlf@gmail.com>
2020-06-11 00:42:44 +0300

    Fix renamer

--- python3/rename.py
+++ python3/rename.py
@@ -10,10 +10,10 @@ def rename(root:str=COVID19RU_ROOT, force:bool=False)->None:
   for root, dirs, filenames in walk(abspath(root), topdown=True):
     for filename in sorted(filenames):
       if filename.endswith('csv'):
-        with open(join(root,filename), 'r') as f:
+        with open(join(root,filename), 'rb') as f:
           s=f.read()
         for en,_,en2 in REGIONS:
-          s=s.replace(en,en2)
-        with open(join(root,filename), 'w') as f:
+          s=s.replace(en.encode(),en2.encode())
+        with open(join(root,filename), 'wb') as f:
           f.write(s)
   return
