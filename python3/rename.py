2510b54262c9410fb2d8140a684d34af7ca2d7c0
Sergey Mironov <grrwlf@gmail.com>
2020-06-11 01:18:31 +0300

    Fix combined key

--- python3/rename.py
+++ python3/rename.py
@@ -18,6 +18,18 @@ def rename(root:str=COVID19RU_ROOT, force:bool=False)->None:
           f.write(s)
   return
 
+def rename_combined_key(root:str=COVID19RU_ROOT, force:bool=False)->None:
+  for root, dirs, filenames in walk(abspath(root), topdown=True):
+    for filename in sorted(filenames):
+      if filename.endswith('csv'):
+        with open(join(root,filename), 'rb') as f:
+          s=f.read()
+        for en,_,en2 in REGIONS:
+          s=s.replace(f"\"{en2},Russia\"".encode(),f"\"{en2}, Russia\"".encode())
+        with open(join(root,filename), 'wb') as f:
+          f.write(s)
+  return
+
 
 def rename_crimea(root:str=COVID19RU_ROOT, force:bool=False):
   for root, dirs, filenames in walk(abspath(root), topdown=True):
